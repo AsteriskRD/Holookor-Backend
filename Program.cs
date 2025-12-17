c70afde201ccf57f98537c0a08cf6eaca7525fe4
Lawson Technology <fatiulawal85@gmail.com>
2025-12-17 23:57:10 +0100

    swagger

--- Program.cs
+++ Program.cs
@@ -107,21 +107,16 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (!app.Environment.IsProduction())
-{
-    app.UseHttpsRedirection();
-}
 
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(c =>
-//    {
-//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Holookor API v1");
-//        c.RoutePrefix = string.Empty;
-//    });
-//}
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Holookor API v1");
+        c.RoutePrefix = string.Empty;
+    });
+
 app.UseHealthChecks("/health");
 
 app.UseCors("AllowAll");
